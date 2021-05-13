1de44494eeb7943adaba8f1b7441eacd758ab34f
Luis C. Garcia Peraza Herrera <luiscarlos.gph@gmail.com>
2021-05-13 22:35:15 +0000

    Adding long description to setup.py.

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@
 import setuptools
 import unittest
 
+# Read the contents of the README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setuptools.setup(name='endoseg',
     version='0.3.0',
     description='Python module to segment the visible circular area of endoscopic images.',
@@ -13,5 +19,7 @@ setuptools.setup(name='endoseg',
     packages=['endoseg'],
     package_dir={'endoseg' : 'src'}, 
     install_requires = ['numpy', 'opencv-python'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     #test_suite = 'tests',
 )
